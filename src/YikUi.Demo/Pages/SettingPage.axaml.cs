ae4b6611d1ead214d4eedd80a07b5c55f87823a1
yiikooo <i@yik.at>
2026-03-14 02:31:04 +0800

    feat(demo): 添加设置页面功能并优化颜色选择器样式
    - 在ColorPicker控件中添加Foreground属性绑定以保持文本颜色一致性
    - 将设置页面的数据上下文改为独立的Setting类实现
    - 添加主题变体、语言和主题颜色的选择功能
    - 使用ColorPicker替换原有的文本框进行颜色选择
    - 调整SplitButton和DropDownButton中的图标宽度以改善视觉效果
    - 为Flyout添加默认内边距样式以提升用户体验

--- src/YikUi.Demo/Pages/SettingPage.axaml.cs
+++ src/YikUi.Demo/Pages/SettingPage.axaml.cs
@@ -1,10 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
-using Avalonia;
-using Avalonia.Input;
-using Avalonia.Media;
-using Avalonia.Styling;
-
 namespace YikUi.Demo.Pages;
 
 public partial class SettingPage : PageModelBase
@@ -12,38 +5,6 @@ public partial class SettingPage : PageModelBase
     public SettingPage()
     {
         InitializeComponent();
-        Application.Current!.ActualThemeVariantChanged += (_, _) =>
-        {
-            SelectedThemeVariant = Application.Current.RequestedThemeVariant ?? ThemeVariant.Default;
-        };
-        DataContext = this;
-    }
-
-    public ThemeVariant SelectedThemeVariant
-    {
-        set
-        {
-            SetField(ref field, value);
-            Application.Current!.RequestedThemeVariant = value;
-        }
-        get;
-    } = ThemeVariant.Default;
-
-    public List<ThemeVariant> ThemeVariants { get; } =
-    [
-        ThemeVariant.Default,
-        ThemeVariant.Light,
-        ThemeVariant.Dark,
-    ];
-
-    private void InputElement_OnKeyDown(object? sender, KeyEventArgs e)
-    {
-        if (e.Key is Key.Enter)
-        {
-            var yikTheme = Application.Current?.Styles
-                .OfType<YikUiTheme>()
-                .FirstOrDefault();
-            yikTheme?.SetThemeColor(Color.Parse(ThemeColor.Text!));
-        }
+        DataContext = Setting.Instance;
     }
 }
