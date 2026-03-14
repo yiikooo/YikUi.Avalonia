7fd4c9b3f7840b308ef97b0bf4dfa8185d0f4a8a
yiikooo <i@yik.at>
2026-03-14 18:06:29 +0800

    feat(theme): 添加按钮组组件和图标按钮扩展
    - 新增 ButtonGroup 组件及其样式定义
    - 实现 IconDropDownButton、IconRepeatButton、IconSplitButton 等图标按钮变体
    - 添加 IconToggleButton 和 IconToggleSplitButton 控件
    - 更新 BindingExtension 中的绑定类型参数
    - 在主题资源字典中注册 ButtonGroup.axaml

---
+++ src/YikUi/Controls/Buttons/ButtonGroup.cs
@@ -0,0 +1,70 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Metadata;
+using YikUi.Common.Helpers;
+
+namespace YikUi.Controls;
+
+public class ButtonGroup : ItemsControl
+{
+    public static readonly StyledProperty<IBinding?> CommandBindingProperty =
+        AvaloniaProperty.Register<ButtonGroup, IBinding?>(
+            nameof(CommandBinding));
+
+    public static readonly StyledProperty<IBinding?> CommandParameterBindingProperty =
+        AvaloniaProperty.Register<ButtonGroup, IBinding?>(
+            nameof(CommandParameterBinding));
+
+    public static readonly StyledProperty<IBinding?> ContentBindingProperty =
+        AvaloniaProperty.Register<ButtonGroup, IBinding?>(
+            nameof(ContentBinding));
+
+    [AssignBinding]
+    [InheritDataTypeFromItems(nameof(ItemsSource))]
+    public IBinding? CommandBinding
+    {
+        get => GetValue(CommandBindingProperty);
+        set => SetValue(CommandBindingProperty, value);
+    }
+
+    [AssignBinding]
+    [InheritDataTypeFromItems(nameof(ItemsSource))]
+    public IBinding? CommandParameterBinding
+    {
+        get => GetValue(CommandParameterBindingProperty);
+        set => SetValue(CommandParameterBindingProperty, value);
+    }
+
+    [AssignBinding]
+    [InheritDataTypeFromItems(nameof(ItemsSource))]
+    public IBinding? ContentBinding
+    {
+        get => GetValue(ContentBindingProperty);
+        set => SetValue(ContentBindingProperty, value);
+    }
+
+
+    protected override bool NeedsContainerOverride(object? item, int index, out object? recycleKey)
+    {
+        recycleKey = null;
+        return item is not Button;
+    }
+
+    protected override Control CreateContainerForItemOverride(object? item, int index, object? recycleKey)
+    {
+        return new Button();
+    }
+
+    protected override void PrepareContainerForItemOverride(Control container, object? item, int index)
+    {
+        base.PrepareContainerForItemOverride(container, item, index);
+        if (container is Button button)
+        {
+            button.TryBind(Button.CommandProperty, CommandBinding);
+            button.TryBind(Button.CommandParameterProperty, CommandParameterBinding);
+            button.TryBind(ContentControl.ContentProperty, ContentBinding);
+            button.TryBind(ContentControl.ContentTemplateProperty, this[!ItemTemplateProperty]);
+        }
+    }
+}
